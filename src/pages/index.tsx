b12fd1f64e172ce1bcbb6220b233a3d1d5dbfe66
Luis V. Capelletto <thecapellett@gmail.com>
2021-12-05 12:37:25 -0300

    initial config

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+import Image from "next/image";
+
+import styles from "../styles/Home.module.scss";
+
+export default function Home() {
+  return (
+    <div>
+      <h2>Rick & Morty Wiki</h2>
+    </div>
+  );
+}
